e06d9595c8cb645c4e773dbbcd950a561ec1bcb4
Joseph Wortmann <jwortmann@quinovas.com>
2022-11-16 10:10:08 -0600

    Use python to publish layer

---
+++ publish_layers.py
@@ -0,0 +1,32 @@
+from os import environ
+import json
+
+import boto3
+
+echocore_zip = None
+with open("echocore.zip", "rb") as f:
+    echocore_zip = f.read()
+
+layer_name = f'echocore-{environ["VERSION"]}'
+echocore_arns = dict()
+for region_name in ("us-east-1", "us-east-2", "us-west-1", "us-west-2"):
+    print(f"Publishing {layer_name} to {region_name}")
+    lambda_client = boto3.client("lambda", region_name=region_name)
+    response = lambda_client.publish_layer_version(
+        CompatibleArchitectures=["x86_64"],
+        CompatibleRuntimes=["python3.9"],
+        Content=dict(ZipFile=echocore_zip),
+        Description="Core Python packages for EchoStream lambda functions",
+        LayerName=layer_name,
+        LicenseInfo="APL2",
+    )
+    lambda_client.add_layer_version_permission(
+        Action="lambda:GetLayerVersion",
+        LayerName=response["LayerArn"],
+        Principal="*",
+        StatementId="PublicAccess",
+        VersionNumber=response["Version"],
+    )
+    echocore_arns[region_name] = response["LayerVersionArn"]
+with open("echocore.json", "wt") as f:
+    json.dump(echocore_arns, f, separators=(",", ":"))
